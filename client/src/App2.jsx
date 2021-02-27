1015618792a9effe216c962eb612a52b7280f551
Jimmy Lin <jimmy.lin0402@gmail.com>
2021-02-27 09:59:36 -0800

    Typewriter effect is functioning, working on media query for front page.

--- client/src/App2.jsx
+++ client/src/App2.jsx
@@ -11,6 +11,7 @@ class App extends React.Component {
   constructor(props){
     super(props)
   }
+  //use componentDidMount() to render loading until state is updated with 'accept';
   render() {
     return(
       <Router>
